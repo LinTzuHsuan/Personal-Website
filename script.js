f75061ce2c2a6cbc692617f040cbc5a3c657a4e0
Bill Lin <lintzuhs@usc.edu>
2021-07-20 17:11:46 +0800

    add files

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "USC Student"],
+        strings: ["Programmer", "CS Student @ USC"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "USC Student"],
+        strings: ["Programmer", "CS Student @ USC"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
